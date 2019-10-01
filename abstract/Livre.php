bd31878d1d1e45c3a47c44ff4ba7e7ef94896fc1
pitichampi <pitichampi@gmail.com>
2019-10-01 09:49:25 +0200

    Un peu de fun en attendant les trainards :p

--- abstract/Livre.php
+++ abstract/Livre.php
@@ -8,9 +8,19 @@ class Livre extends SupportDeCours
 {
     public function imprimer()
     {
+        $lwhitet=27-strlen($this->titre);
+        $lwhitea=27-strlen($this->auteur);
+        $whitet='';
+        for ($i=0; $i<$lwhitet;$i++){
+            $whitet.='&nbsp;';
+        }
+        $whitea='';
+        for ($i=0; $i<$lwhitea;$i++){
+            $whitea.='&nbsp;';
+        }
         echo '**** IMPRESSION EN COURS *****<br>';
-        echo '* '.$this->titre.'<br>';
-        echo '* '.$this->auteur.'<br>';
+        echo '* '.$this->titre.$whitet.'*<br>';
+        echo '* '.$this->auteur.$whitea.'*<br>';
         echo '******************************<br>';
     }
 }
--- abstract/index.php
+++ abstract/index.php
@@ -4,7 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        html{
+            font-family: 'Source Code Pro', monospace;
+        }
+    </style>
     <title>Document</title>
 </head>
 <body>
