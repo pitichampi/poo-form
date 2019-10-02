08cacb73db31cdcad0ba6a3cfa59f1f988dcf685
pitichampi <pitichampi@gmail.com>
2019-10-02 08:51:44 +0200

    y avait un truc qui allait pas dans le scénar...

--- sql/index.php
+++ sql/index.php
@@ -1,1 +1,28 @@
 <?php
+$pdo =  new PDO('mysql:host=127.0.0.1;dbname=poo','root','123456');
+    ?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pwet</title>
+</head>
+<body>
+<?php
+$sql='SELECT * FROM users WHERE login=:login';
+$stmt = $pdo->prepare($sql);
+$login='pouet';
+$stmt->bindParam(':login',$login);
+$stmt->execute();
+
+$row = $stmt->fetch();
+echo $row['email'];
+error_log('Tuuuut');
+?>
+
+
+</body>
+</html>
